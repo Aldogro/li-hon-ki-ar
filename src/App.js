80c5f71c5c15fde85fbc5fb11c218e0ec56cb3c9
Aldo Rojas <aldogrojas5@gmail.com>
2023-10-27 11:02:03 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import logo from './assets/li-hon-ki-logo.png';
+
+function App() {
+  return (
+    <div className="main">
+      <img src={logo} height="400" alt="school-logo" />
+      <h1>En construcción...</h1>
+    </div>
+  );
+}
+
+export default App;
