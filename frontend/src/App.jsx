8eeb4e18c419e1d34c92dd37d61a63d91755cc0c
Aldo Rojas <aldogrojas5@gmail.com>
2023-11-14 16:16:58 -0300

    modifications on loading component and main layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MainLayout from './MainLayout';
 import LoadingComponent from './components/LoadingComponent';
+import StickyHeader from './components/StickyHeader'
 
 function App() {
   return (
     <BrowserRouter>
+      <StickyHeader />
       <Suspense fallback={<LoadingComponent />}>
         <MainLayout />
       </Suspense>
