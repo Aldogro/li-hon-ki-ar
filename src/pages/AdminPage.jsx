f1906aa1fb645156c9d0902a9d8e5b334c80f076
AldogroTRU <aldo@trusolutions.com>
2025-03-22 10:02:48 -0300

    added loading component to Admin's page list of users

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,10 +4,11 @@ import { collection, firestore } from '../firebase/firebase';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 import AdminUserInfo from '../components/AdminUserInfo';
 import './AdminPage.css';
+import LoadingComponent from '../components/LoadingComponent';
 
 const AdminPage = () => {
 
-    const [users] = useCollectionData(
+    const [users, loading] = useCollectionData(
         collection(firestore, 'users'),
         {
           snapshotListenOptions: { includeMetadataChanges: true },
@@ -23,9 +24,9 @@ const AdminPage = () => {
             <h2>Usuarios Registrados</h2>
             <div className="admin-page_user-list">
                 {
-                    users?.map((user) => (
-                        <AdminUserInfo key={user.uid} user={user} />
-                    ))
+                    loading
+                        ? <LoadingComponent />
+                        : users?.map((user) => (<AdminUserInfo key={user.uid} user={user} />))
                 }
             </div>
         </CommonContainer>
